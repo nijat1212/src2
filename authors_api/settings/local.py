555ffc46538000e91395662bff0f2aedcf4d660e
nijat1212 <nicatagazamanov@gmail.com>
2023-09-25 13:05:13 +0300

    zm: Gr

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -10,4 +10,11 @@ SECRET_KEY = env(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@nicatagazamanov@gmail.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Authors Haven"
--- core_apps/profiles/models.py
+++ core_apps/profiles/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+from core_apps.common.models import TimeStampedModel
 
-# Create your models here.
+User = get_user_model()
+
+class Profile(TimeStampedModel):
+    class Gender(models.TextChoices):
+        MALE = ("M", _("Male"))
+        FEMALE = ("F", _("Female"))
+        OTHER = ("O", _("Other"))
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    phone_number = PhoneNumberField(verbose_name=_("phone number"), max_length=30, default="+994123456789")
+    about_me = models.TextField(verbose_name=_("about me"), default="Say something about yourself")
+    gender = models.CharField(
+        verbose_name=_("gender"),
+        choices=Gender.choices,
+        default=Gender.OTHER,
+        max_length=20,
+    )
+    country = CountryField(
+        verbose_name=_("country"),
+        default="KE",
+        blank=False,
+        null=False,
+    )
+    city = models.CharField(
+        verbose_name=_("city"),
+        max_length=180,
+        default="Nairobi",
+        blank=False,
+        null=False,
+    )
+    profile_photo = models.ImageField(
+        verbose_name=_("profile photo"),
+        upload_to="profile_photos/",
+        default="profile_default.png",
+    )
+    twitter_handle = models.CharField(
+        verbose_name=_("twitter handle"),
+        max_length=20,
+        blank=True,
+    )
+    followers = models.ManyToManyField(
+        "self",
+        symmetrical=False,
+        related_name="following",
+        blank=True,
+    )
+
+
+    def __str__(self):
+        return self.user.get_full_name
+
+
+    def follow(self, profile):
+        self.followers.add(profile)
+
+    def unfollow(self, profile):
+        self.followers.remove(profile)
+
+    def check_following(self, profile):
+        return self.followers.filter(pk=profile.pk).exists()
